10010ef632276e0246158a1504fec47a03143df5
Ritesh Singh <riteshsingh86992@gmail.com>
2025-10-04 12:46:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Jenkins CI/CD + Docker + Node.js!");
+  res.send("🚀 Hello from Jenkins CI/CD + Docker + Node.js! this is Ritesh");
 });
 
 app.listen(PORT, () => {
